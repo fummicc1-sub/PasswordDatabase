a14bdff7b9b47e96e777b5af6a23a0ea199fb501
fummicc1-sub <fummiya989@gmail.com>
2018-12-03 11:46:00 +0900

    Initial Commit

---
+++ PasswordDatabase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PasswordDatabase
+//
+//  Created by Fumiya Tanaka on 2018/12/03.
+//  Copyright © 2018 Fumiya Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ PasswordDatabaseTests/PasswordDatabaseTests.swift
@@ -0,0 +1,34 @@
+//
+//  PasswordDatabaseTests.swift
+//  PasswordDatabaseTests
+//
+//  Created by Fumiya Tanaka on 2018/12/03.
+//  Copyright © 2018 Fumiya Tanaka. All rights reserved.
+//
+
+import XCTest
+@testable import PasswordDatabase
+
+class PasswordDatabaseTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
